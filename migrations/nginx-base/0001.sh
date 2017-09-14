1d6b0a805d3ce150113e741524e6784f7e344c61
Konstantin Kovshenin <kovshenin@gmail.com>
2017-09-14 15:41:31 -0700

    Enable nginx.service prior to starting/restarting it.

--- migrations/nginx-base/0001.sh
+++ migrations/nginx-base/0001.sh
@@ -10,5 +10,6 @@ ln -sf /usr/local/sbin/nginx-lb /usr/local/sbin/nginx
 # Start on boot
 update-rc.d nginx defaults
 
+/bin/systemctl enable nginx.service
 /usr/sbin/service nginx restart
 /usr/sbin/service nginx start
