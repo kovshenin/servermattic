31fe84a8d54c0a9e0b8264b73be83805eaf7cf00
Michael Turk <turk.michael@gmail.com>
2017-09-15 15:49:15 -0700

    updating localhost

--- migrations/wordpress-db-primary/0001.sh
+++ migrations/wordpress-db-primary/0001.sh
@@ -11,6 +11,6 @@ cd /usr/local/mysql5.6/
 chown -R mysql.mysql /var/lib/mysql1-0
 /etc/init.d/mysql1-0 start
 /usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql1-0.cnf -u root password '9^Ng~$X?kWGytn3t'
-mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -p'9^Ng~$X?kWGytn3t' -e "create user 'wp'@'localhost' IDENTIFIED BY '24qTDLAH]hv2dM';"
+mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -p'9^Ng~$X?kWGytn3t' -e "create user 'wp'@'%' IDENTIFIED BY '24qTDLAH]hv2dM';"
 mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -p'9^Ng~$X?kWGytn3t' -e "create database wp;"
 mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -p'9^Ng~$X?kWGytn3t' -e "GRANT ALL ON wp.* TO 'wp'@'%';"
