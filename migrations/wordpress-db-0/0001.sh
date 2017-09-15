7fd9fdce0e478d07e05fbfa655370d13bda4c565
Michael Turk <turk.michael@gmail.com>
2017-09-15 12:59:48 -0700

    bad commit message

---
+++ migrations/wordpress-db-0/0001.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Setup MySQL
+ln -sf /etc/mysql-initscripts/mysql-5.6 /etc/init.d/mysql1-0
+
+mkdir -p /var/log/mysql1-0
+chown mysql.mysql /var/log/mysql1-0
+cd /usr/local/mysql5.6/
+./scripts/mysql_install_db --defaults-file=/etc/mysql/mysql1-0.cnf --user=mysql --datadir=/var/lib/mysql1-0 --force  --skip-name-resolve
+chown -R mysql.mysql /var/lib/mysql1-0
+/etc/init.d/mysql1-0 start
+/usr/local/mysql/bin/mysqladmin --defaults-file=/etc/mysql/mysql1-0.cnf -u root password '9^Ng~$X?kWGytn3t'
+mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "create user 'wp'@'localhost' IDENTIFIED BY '24qTDLAH]hv2dM';"
+mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "create database wp;"
+mysql --defaults-file=/etc/mysql/mysql1-0.cnf -u root -sN  -pgod -e "GRANT ALL ON wp.* TO 'wp'@'localhost';"
