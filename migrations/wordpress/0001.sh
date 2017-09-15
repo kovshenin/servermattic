51ce234cf2c9edb55c90257da26043fa26121377
Valerie <valerie@automattic.com>
2017-09-15 15:53:59 -0700

    Add hyperdb drop-in

--- migrations/wordpress/0001.sh
+++ migrations/wordpress/0001.sh
@@ -3,3 +3,6 @@
 wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress-latest.tar.gz
 tar -xf /tmp/wordpress-latest.tar.gz -C /var/www/wordpress --strip-components=1
 rm /tmp/wordpress-latest.tar.gz
+
+# install hyperdb
+wget https://plugins.svn.wordpress.org/hyperdb/trunk/db.php -O /var/www/wordpress/wp-content/db.php
