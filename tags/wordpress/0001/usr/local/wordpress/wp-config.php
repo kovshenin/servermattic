174c43dd062a05650b4860f0eb71da9b77f8e186
Konstantin Kovshenin <kovshenin@gmail.com>
2017-09-14 17:06:30 -0700

    Set db credentials in wp-config.php

--- tags/wordpress/0001/usr/local/wordpress/wp-config.php
+++ tags/wordpress/0001/usr/local/wordpress/wp-config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_NAME', 'database_name_here');
-define('DB_USER', 'username_here');
-define('DB_PASSWORD', 'password_here');
+define('DB_NAME', 'wp');
+define('DB_USER', 'wp');
+define('DB_PASSWORD', 'drupal');
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
