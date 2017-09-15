9e871c14ad733efc9168afa0aa6497a84af5fab9
Konstantin Kovshenin <kovshenin@gmail.com>
2017-09-15 16:18:47 -0700

    Create /var/www/wordpress

--- migrations/wordpress-web/0001.sh
+++ migrations/wordpress-web/0001.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -r /var/www/wordpress
 /usr/sbin/service nginx reload
 wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress-latest.tar.gz
 tar -xf /tmp/wordpress-latest.tar.gz -C /var/www/wordpress --strip-components=1
