e192312242178439774d0e40ba72f9c11dc42b43
Konstantin Kovshenin <kovshenin@gmail.com>
2017-09-15 16:19:42 -0700

    It's -p not -r

--- migrations/wordpress-web/0001.sh
+++ migrations/wordpress-web/0001.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -r /var/www/wordpress
+mkdir -p /var/www/wordpress
 /usr/sbin/service nginx reload
 wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress-latest.tar.gz
 tar -xf /tmp/wordpress-latest.tar.gz -C /var/www/wordpress --strip-components=1
