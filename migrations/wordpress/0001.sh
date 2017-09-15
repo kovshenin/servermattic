3684b3758035b93e58e148047fa2a0954d46b21b
Valerie <valerie@automattic.com>
2017-09-15 15:40:50 -0700

    Remove wordpress tar file after installing

--- migrations/wordpress/0001.sh
+++ migrations/wordpress/0001.sh
@@ -2,3 +2,4 @@
 /usr/sbin/service nginx reload
 wget https://wordpress.org/latest.tar.gz -O /tmp/wordpress-latest.tar.gz
 tar -xf /tmp/wordpress-latest.tar.gz -C /var/www/wordpress --strip-components=1
+rm /tmp/wordpress-latest.tar.gz
